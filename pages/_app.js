68c633a249a8f90199859f71ee078a6e4db9f028
malanski <malanskiwork@gmail.com>
2022-03-14 18:30:03 -0300

    Add direct APi route using poke number id

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.scss'
 import '../styles/Header.scss'
 import '../styles/Aside.module.scss'
+import '../styles/Layout.module.scss'
 import '../styles/Footer.scss'
 import { ChakraProvider } from '@chakra-ui/react'
 
--- pages/index.js
+++ pages/index.js
@@ -1,103 +1,97 @@
 import Layout from '../components/layout'
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
-import Image from 'next/image'
-import Pikachu from '../public/pikachu-01.png'
-import { IconButton, Button } from '@chakra-ui/react'
-import { ArrowLeftIcon, ArrowRightIcon} from '@chakra-ui/icons'
 import Link from 'next/link'
-
-import { PokeApi } from './api/pokeApi'
+import styles from '../styles/Home.module.scss'
+import Head from 'next/head'
+import { Button, ButtonGroup, IconButton } from '@chakra-ui/react'
+import { ArrowLeftIcon, ArrowRightIcon, BellIcon, CheckCircleIcon, EmailIcon } from '@chakra-ui/icons'
 import { useEffect, useState } from 'react'
+import Pokemon from './pokemon/[id]'
+import {PokeApi} from './api/pokeApi'
 
-const Pokemon = (props) => {
-  const name = props.name;
-  const id = props.url.replace('https://pokeapi.co/api/v2/pokemon/', '').replace('/','')
-  const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
-  return (
-    <Link href='/pokemon/' as={name}>
-      <a className={styles.card}>                                     
-        <h2>{name}</h2>
-        <img src={imageUrl} alt={name}/>
-        <p className={styles.price}>150.00R$</p>
-        <p className={styles.priceDisc}>120.00R$</p>
-        
-        <Button className={styles.buyBtn}>
-          <span>Buy</span>
-        </Button>
-      </a>
-    </Link>
-  )
-}
-
-export default function Home(props) {
-  const limit = 20 
-
+export default function Home({ pokemon }) {
+  const limit = 20
   const [state, setState] = useState({
     loading: true, 
     pokemons: [],
     total: 0,
-    currentPage: 0, 
+    currentPage: 0,
     totalPages: 0,
   })
   useEffect(() => {
     PokeApi.listPokemons(state.currentPage * limit, limit).then(({data}) => {
       setState((prev) => ({
         ...prev,
-        loading: false,
+        loading: false, 
         total: data.count,
         totalPages: Math.ceil(data.count / limit),
         pokemons: [...prev.pokemons, ...data.results.map((pokemon, key) => <Pokemon key={key + (prev.pokemons.length+1)} name={pokemon.name} url={pokemon.url} />)]
       }))
     })
   }, [state.currentPage])
-
-  const loadMore = () => {
-    let page = state.currentPage
-
-    if ((page + 1) >= state.totalPages) return
-
-    page +=1
-    setState((prev) => ({
-      ...prev,
-      currentPage: page,
-    }))
-  }
-
-  const loadLess = () => {
-    let page = state.currentPage
-
-    if ((page - 1) >= state.totalPages) return
-
-    page -=1
-    setState((prev) => ({
-      ...prev,
-      currentPage: page,
-    }))
-  }
   return (
-    <Layout>
-        <Head>
-          
-          <title>Poke-Store Next App</title>
-                    
-        </Head>
+    <Layout title="NextJS PokeDex">
 
-        <div className={styles.mainsite}>
+      <Head>
+        <title>Poke-Store Next.js Project</title>
+      </Head>
 
-          <div className={styles.pagination}>            
-            <IconButton onClick={loadLess} aria-label='Preview' icon={state.currentPage < state.totalPages &&<ArrowLeftIcon />} />
+      
 
-            <IconButton onClick={loadMore} aria-label='Next' icon={state.currentPage < state.totalPages &&<ArrowRightIcon />} />
-          </div>
+        <ul className={styles.grid}>
 
-          <div className={styles.grid}>
-           {state.pokemons}
-          </div>
+          {pokemon.map((item, index) => (
+              <li key={index} className={styles.pokelist} >
 
+                  <Link href={`/pokemon/${index + 1}`}>
+                      <a className={styles.card}>
+
+                        <h2 className={styles.pokename}>{item.name}</h2>
+
+                        <img
+                            src={item.image}
+                            alt={item.name}
+                            className={styles.pokeImg}
+                        />
+
+                        <p className={styles.price}>150.00$</p>
+                        <p className={styles.priceDisc}>119.99$</p>
+                        
+                        
+                        <Button className={styles.buybtn}
+                        leftIcon={<CheckCircleIcon  />}
+                        iconSpacing='50' colorScheme='teal' variant='solid'>
+                        Buy
+                        </Button>
+
+                      </a>
+                  </Link>
+              </li>
+            ))}
+        </ul>
+
+        <div className={styles.pagination}>
+          <IconButton aria-label='Preview' icon={<ArrowLeftIcon />} />
+          <IconButton aria-label='Preview' icon={<ArrowRightIcon />} />
         </div>
 
 
+        
     </Layout>
   )
 }
+
+export const getStaticProps = async () => {
+  
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150');
+    const { results }  = await res.json();
+    const pokemon = results.map((pokeMons, index) => {
+        const paddedId = ('00' + (index + 1)).slice(-3);
+        const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`;
+        return { ...pokeMons, image };
+    });
+    return {
+        props: { pokemon },
+    };
+
+}
+
